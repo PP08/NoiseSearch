3e1a99faf4270a0fb85811cc63f943f71580edeb
phucphuong <phucphuong1293@gmail.com>
2017-03-15 23:29:48 +0300

    Create new windows for settings and info

--- app/src/main/java/com/phucphuong/noisesearch/Activities/MainActivity.java
+++ app/src/main/java/com/phucphuong/noisesearch/Activities/MainActivity.java
@@ -1,7 +1,10 @@
 package com.phucphuong.noisesearch.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.phucphuong.noisesearch.R;
 
@@ -11,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btn_function1 = (ImageButton)findViewById(R.id.btn_function1);
+        ImageButton btn_function2 = (ImageButton)findViewById(R.id.btn_function1);
+
+
+
+
+        btn_function1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MeasureAtSinglePoint.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/phucphuong/noisesearch/Fragments/SettingsFragment.java
+++ app/src/main/java/com/phucphuong/noisesearch/Fragments/SettingsFragment.java
@@ -3,11 +3,13 @@ package com.phucphuong.noisesearch.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.phucphuong.noisesearch.R;
@@ -25,7 +27,7 @@ public class SettingsFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View settingsView = inflater.inflate(R.layout.fragment_settings, container, true);
@@ -37,7 +39,8 @@ public class SettingsFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(getActivity(), "You've clicked settings button", Toast.LENGTH_SHORT).show();
+                View mview = inflater.inflate(R.layout.setting_window, container, true);
+                showAlertDialog(inflater, container, mview);
 
             }
         });
@@ -45,13 +48,38 @@ public class SettingsFragment extends Fragment {
         btn_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "You've clicked info button", Toast.LENGTH_SHORT).show();
+                View mview = inflater.inflate(R.layout.info_window, container, true);
+                showAlertDialog(inflater, container, mview);
             }
         });
 
 
 
         return settingsView;
+    }
+
+    private void showAlertDialog(final LayoutInflater inflater, final ViewGroup container, View mview){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        //View mview = inflater.inflate(R.layout.setting_window, container, true);
+
+        builder.setView(mview);
+        final AlertDialog dialog = builder.create();
+        //1
+        // dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+//        TextView title = (TextView)mview.findViewById(R.id.titleSettings);
+//        Button btn_calinrate = (Button) mview.findViewById(R.id.btn_calibrate);
+//        Button btn_filemanager = (Button) mview.findViewById(R.id.btn_file);
+        ImageButton btn_close = (ImageButton) mview.findViewById(R.id.btn_close);
+
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
     }
 
 }
