d9d268625050382c26217d9bb5fe83019eff1630
phucphuong <phucphuong1293@gmail.com>
2017-05-30 19:08:16 +0300

    show the date and time dialog when the app started

--- app/src/main/java/com/phucphuong/noisesearch/Activities/MainActivity.java
+++ app/src/main/java/com/phucphuong/noisesearch/Activities/MainActivity.java
@@ -1,6 +1,9 @@
 package com.phucphuong.noisesearch.Activities;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -11,9 +14,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.phucphuong.noisesearch.Fragments.MeterFragment;
 import com.phucphuong.noisesearch.R;
 import com.phucphuong.noisesearch.Utilities.ReplaceFont;
+import com.phucphuong.noisesearch.Utilities.UploadFile;
+
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,6 +63,40 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent2);
             }
         });
+
+
+        //Toast.makeText(this, "hello there", Toast.LENGTH_LONG).show();
+
+        //show the datetime dialog here!
+
+        //get system date and time
+
+        final Calendar c = Calendar.getInstance();
+        String year = Integer.toString(c.get(Calendar.YEAR));
+        String month = Integer.toString(c.get(Calendar.MONTH));
+        String day = Integer.toString(c.get(Calendar.DAY_OF_MONTH));
+        String hour = Integer.toString(c.get(Calendar.HOUR_OF_DAY));
+        String sminute;
+        int minute = c.get(Calendar.MINUTE);
+        if (minute < 10){
+            sminute = "0" + Integer.toString(minute);
+        }else {
+            sminute = Integer.toString(minute);
+        }
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Please check your date and time");
+        alert.setMessage("Date: " + day + " - " + month +
+        " - " + year + "\n\nTime: "+ hour + " : " + sminute);
+        alert.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        alert.show();
+
 
     }
 
